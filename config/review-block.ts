7ce3574c8dc0e6f758c986ffe93c9490db8d5f30
qa921 <qa@agent51testing.com>
2026-09-15 12:38:06 +0530

    seed M-T658-V1 block config

---
+++ config/review-block.ts
@@ -0,0 +1,9 @@
+export const reviewBlockConfig = {
+  label: 'M-T658-V1',
+  blockId: 'BR-APR-2026-A',
+  workbookName: 'M-T658-V1 Coach Review Source.xlsx',
+  expectedSheet: 'KPI_Source',
+  sourceSchema: 'm_t658_v1',
+  staleCompetitionLabel: 'National Premier League',
+  currentSlideOrder: ['title', 'seasonTotals', 'topScorers', 'notes'],
+};
